64cab91f1bff89112595f765f2184f420dfc7231
kplakosh <kplakosh@gmail.com>
2018-12-25 23:55:05 -0800

    API call for recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
 // Global app controller
 
-import string from './models/Search';
-// import {add as a , multiply, ID} from './view/searchView';
-import * as searchView from './view/searchView';
+import axios from 'axios';
 
-console.log(` Using imported functions ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${string}`);
+async function getResults(query) {
+    const key = '39ad21d0719c6401ffd743ecd7c541ee';
+    try {
+        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+}
+
+getResults('pizza');
+
+// https://www.food2fork.com/api/search
+// 39ad21d0719c6401ffd743ecd7c541ee
+
