46da85c0aacee2f3e529909e2733fdd32bdf4dba
kplakosh <kplakosh@gmail.com>
2018-12-26 00:07:39 -0800

    search model added

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,8 @@
 // Global app controller
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '39ad21d0719c6401ffd743ecd7c541ee';
-    try {
-        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
-    }
-}
+const search = new Search('pizza');
+console.log(search);
 
-getResults('pizza');
-
-// https://www.food2fork.com/api/search
-// 39ad21d0719c6401ffd743ecd7c541ee
-
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am an exported string'
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+    const key = '39ad21d0719c6401ffd743ecd7c541ee';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+        this.result = res.data.recipes;
+        // console.log(this.result);
+    } catch (error) {
+        alert(error);
+    }
+}
+}
+
