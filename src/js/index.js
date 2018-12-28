2795abc60342165cc49f6be90db236016feeeebd
kplakosh <kplakosh@gmail.com>
2018-12-28 15:25:49 -0800

    recipe view added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './view/searchView';
+import * as recipeView from './view/recipeView';
 import { elements, renderLoader, clearLoader } from './view/base';
 import Recipe from './models/Recipe';
 
@@ -67,6 +68,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -81,7 +84,8 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (err) {
             alert('Error processing recipe!');
         }
