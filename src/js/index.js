7d59fd2258724abb5c201b30a5d0ba6b4fba2669
kplakosh <kplakosh@gmail.com>
2018-12-29 15:31:38 -0800

    local storage implemented

--- src/js/index.js
+++ src/js/index.js
@@ -144,11 +144,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * LIKE CONTROLLER
  */
-//TESTING
-state.likes = new Likes();
-likesView.toggleLikesMenu(state.likes.getNumLikes());
-
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -183,6 +178,20 @@ const controlLike = () => {
     }
     likesView.toggleLikesMenu(state.likes.getNumLikes());
 };
+
+// Restore like recipe on page loads
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikesMenu(state.likes.getNumLikes());
+
+    // Render existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks
